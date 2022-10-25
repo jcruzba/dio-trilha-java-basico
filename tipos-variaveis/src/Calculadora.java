c369145b73d6b28e0323ced4d93b581fb5d4673e
jcruzba <114691251+jcruzba@users.noreply.github.com>
2022-10-25 19:44:49 -0300

    Calculadora e comentarios

---
+++ tipos-variaveis/src/Calculadora.java
@@ -0,0 +1,22 @@
+/** 
+ * <h1> Calculadora</h1>
+ * A calculadora realiza operações matemáticas entre números inteiros.
+ * <p>
+ * <b>Note:</b> Leia atentamente  a documentação destas classes  para desfrutar  dos recursos oferecidos pelo autor
+ * 
+ * @author José Cruz
+ * @version 1.0
+ * @since 25/10/2022
+*/
+public class Calculadora   {
+    /**
+     * Este método é utilizado  para somar dois numeros inteiros
+     * @param numeroUm este é o primeiro parâmetro do método
+     * @param numeroDois este é o segundo parâmetro do método
+     * @return int o resultado deste método é a soma dos dois números
+     */
+public int somar ( int numeroUm, int numeroDois) {
+    return  numeroUm + numeroDois;
+}
+     
+}
---
+++ tipos-variaveis/src/Comentarios.java
@@ -0,0 +1,22 @@
+public class Comentarios {
+    public static void main(String[] args) {
+        // Olá, duas barras para comentário de uma linha
+
+
+        /* Olá, Uma barra e um asterisco,
+        * É um comentário mais detalhado
+        * quando necessário
+        */
+    }
+    /**
+     * Estas duas estrelinhas acima com a barra
+     * é para identificar  que você pretende
+     * elabora uma comentário a nivel
+     * de documentação.
+     * Que incrível !!
+     */
+
+    public void metodo(){
+
+    }
+}
